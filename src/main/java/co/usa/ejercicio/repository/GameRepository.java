ae2074853e2616e66c9bbb712a7f65c5d2659739
DavidSArbelaez <108475569+DavidSArbelaez@users.noreply.github.com>
2023-04-02 18:25:42 +0000

    Errores y mas erreoes por corregir

--- src/main/java/co/usa/ejercicio/repository/GameRepository.java
+++ src/main/java/co/usa/ejercicio/repository/GameRepository.java
@@ -1,5 +1,30 @@
 package co.usa.ejercicio.repository;
 
+
+import com.
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class GameRepository {
-    
+    @Autowired
+    private GameCrudRepository gameCrudRepository;
+
+    public List<Game> findAll() {
+        return (List<Game>) gameCrudRepository.findAll();
+    }
+
+    public Optional<Game> findById(Long id) {
+        return gameCrudRepository.findById(id);
+    }
+
+    public <S extends Game> S save(S game) {
+        return gameCrudRepository.save(game);
+    }
+
+    public void deleteById(Long id) {
+        gameCrudRepository.deleteById(id);
+    }
 }
