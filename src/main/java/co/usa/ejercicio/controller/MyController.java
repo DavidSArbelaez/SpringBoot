fb4d1e7d8f0e63441ab232bd0fd3c66589fa1fd6
DavidSArbelaez <108475569+DavidSArbelaez@users.noreply.github.com>
2023-04-02 16:38:24 +0000

    Cambio de nombre del controlador

--- src/main/java/co/usa/ejercicio/controller/MyController.java
+++ src/main/java/co/usa/ejercicio/controller/MyController.java
@@ -1,12 +1,14 @@
-package co.usa.ejercicio;
+package co.usa.ejercicio.controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/juegos")//ruta dentro de la apppara aceeder al componente
-public class controlador {
+@RestController
+@RequestMapping("/api/juegos")//ruta dentro de la apppara aceeder al componente
+public class MyController {
     
-    @GetMapping
+    @GetMapping("/saludar")
     public String hola(){
-        return "<h1>Hola</h1>";
+        return "<h1>Hola mundo</h1>";
     }
 }
