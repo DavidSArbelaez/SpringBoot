3876c42037a0d48ddfa28dd02395abeb875632e8
DavidSArbelaez <108475569+DavidSArbelaez@users.noreply.github.com>
2023-04-03 02:56:57 +0000

    Agrego cod

--- src/main/resources/static/js/myScript.js
+++ src/main/resources/static/js/myScript.js
@@ -1,1 +1,18 @@
-function 
+function getGames() {
+    $.ajax({
+        url: 'api/games/all',
+        type: 'GET',
+        dataType: 'json', // Se espera una respuesta en formato JSON
+        contentType: 'application/json; charset=utf-8',
+        success: function(data) {
+            // Aquí procesamos los datos obtenidos
+            console.log(data);
+            paintGames(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            // Aquí manejamos cualquier error que pueda haber ocurrido
+            console.log(textStatus + ': ' + errorThrown);
+        }
+    });
+
+}
